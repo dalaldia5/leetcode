cb3673188bb36f1c284812c9cc2a87b66825c10b
Dia Dalal <dia140706@gmail.com>
2025-10-21 22:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (80.64%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        int n = nums1.size();
-        int m = nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
         int i = 0, j = 0;
-        while (i < n && j < m) {
-            if (nums1[i] < nums2[j])
-                ans.push_back(nums1[i++]);
-            else
-                ans.push_back(nums2[j++]);
+        int n = n1 + n2;
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+        int cnt = 0;
+        int ind1el = -1, ind2el = -1;
+        while (i < n1 && j < n2) {
+            if (nums1[i] < nums2[j]) {
+                if (cnt == ind1)
+                    ind1el = nums1[i];
+                if (cnt == ind2)
+                    ind2el = nums1[i];
+                cnt++;
+                i++;
+            } else {
+                if (cnt == ind1)
+                    ind1el = nums2[j];
+                if (cnt == ind2)
+                    ind2el = nums2[j];
+                cnt++;
+                j++;
+            }
         }
-        while (i < n)
-            ans.push_back(nums1[i++]);
-        while (j < m)
-            ans.push_back(nums2[j++]);
-        int s = ans.size();
-        if (s % 2)
-            return ans[s / 2.0];
+        while (i < n1) {
+            if (cnt == ind1)
+                ind1el = nums1[i];
+            if (cnt == ind2)
+                ind2el = nums1[i];
+            cnt++;
+            i++;
+        }
+        while (j < n2) {
+            if (cnt == ind1)
+                ind1el = nums2[j];
+            if (cnt == ind2)
+                ind2el = nums2[j];
+            cnt++;
+            j++;
+        }
+        if (n % 2 == 1)
+            return ind2el;
         else
-            return (ans[s / 2 - 1] + ans[s / 2]) / 2.0;
+            return (ind1el + ind2el) / 2.0;
     }
 };
