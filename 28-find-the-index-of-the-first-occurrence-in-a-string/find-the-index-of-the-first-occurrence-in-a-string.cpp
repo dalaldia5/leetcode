d4fe9c2a057615e26d69f0485da20c6c7226223f
Dia Dalal <dia140706@gmail.com>
2025-11-12 23:32:44 +0530

    Time: 2 ms (9.45%) | Memory: 9 MB (12.62%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,14 +1,36 @@
 class Solution {
 public:
-    int strStr(string s1, string s2) {
-        for (int i = 0; i < s1.size(); i++) {
-            for (int j = 0; j < s2.size(); j++) {
-                if (s1[i + j] != s2[j])
-                    break;
-                if (j == s2.size() - 1)
-                    return i;
+    vector<int> computeZArray(const string& s) {
+        vector<int> z(s.size(), 0);
+        int l = 0, r = 0;
+        for (int i = 1; i < s.size(); i++) {
+            if (i <= r) {
+                z[i] = min(r - i + 1, z[i - l]);
+            }
+            while (i + z[i] < s.size() && s[z[i]] == s[i + z[i]])
+                z[i]++;
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
             }
         }
+        return z;
+    }
+
+    int strStr(string haystack, string needle) {
+        int ans = 0;
+        string zString = needle + "$" + haystack;
+
+        // call and store result properly
+        vector<int> z = computeZArray(zString);
+
+        for (int i = 0; i < z.size(); i++) {
+            if (z[i] == needle.length()) {
+                ans = i - (needle.length() + 1);
+                return ans;
+            }
+        }
+
         return -1;
     }
 };
