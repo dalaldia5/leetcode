7926070681eb103f92d6203b77858ad1398fb6cf
Dia Dalal <dia140706@gmail.com>
2025-12-05 00:17:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (25.13%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -6,35 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    
     void flatten(TreeNode* root) {
-        if (!root)
-            return;
-        vector<TreeNode*> preorder;
-        preorderTraversal(root, preorder);
-        for (int i = 1; i < preorder.size(); i++) {
-            preorder[i - 1]->left = NULL;
-            preorder[i - 1]->right = preorder[i];
-        }
-    }
-    void preorderTraversal(TreeNode* root, vector<TreeNode*>& preorder) {
-        if (root == NULL)
-            return;
-        stack<TreeNode*> st;
+        if (!root) return;  
+        stack<TreeNode*>st;
         st.push(root);
-        while (!st.empty()) {
-            root = st.top();
+        while(!st.empty()){
+            TreeNode*curr=st.top();
             st.pop();
-            preorder.push_back(root);
-            if (root->right != NULL)
-                st.push(root->right);
-            if (root->left != NULL)
-                st.push(root->left);
+            if(curr->right)st.push(curr->right);
+            if(curr->left)st.push(curr->left);
+            if(!st.empty()){
+                curr->right=st.top();
+            }
+            curr->left=NULL;
         }
+        
     }
 };
