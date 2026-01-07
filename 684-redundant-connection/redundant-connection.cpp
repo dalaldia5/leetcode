743d4a325c0400f85fbd386450145ca30b120986
Dia Dalal <dia140706@gmail.com>
2026-01-07 20:25:37 +0530

    Time: 82 ms (5.10%) | Memory: 61.8 MB (5.50%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,36 +1,63 @@
 class Solution {
 public:
-// cycle detection using bfs
-    bool detectCycle(int node, vector<vector<int>>& adj, vector<int>& vis) {
-        queue<pair<int, int>> q;
-        q.push({node, -1});
-        vis[node] = 1;
-        while (!q.empty()) {
-            int src = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            for (auto it : adj[src]) {
-                if (!vis[it]) {
-                    vis[it] = 1;
-                    q.push({it, src});
-                } else if (it != parent) {
-                    return true;
+    //  cycle detection using dsu
+    int find(int u, vector<int>& parent) {
+        if (parent[u] == u) {
+            return u;
+        }
+        return parent[u] = find(parent[u], parent);
+    }
+
+    void dfsUnion(int u, int v, vector<int>& parent, vector<int>& rank) {
+        int u_parent = find(u, parent);
+        int v_parent = find(v, parent);
+        if (u_parent == v_parent)
+            return;
+        if (rank[u_parent] > rank[v_parent]) {
+            parent[v_parent] = u_parent;
+        } else if (rank[u_parent] < rank[v_parent]) {
+            parent[u_parent] = v_parent;
+        } else {
+            parent[u_parent] = v_parent;
+            rank[v_parent]++;
+        }
+    }
+
+    bool cycleDetect(int node, int V, vector<int>& vis,
+                     vector<vector<int>>& adj) {
+        vector<int> parent(V+1);
+        for (int i = 1; i <= V; i++) {
+            parent[i] = i;
+        }
+        vector<int> rank(V+1, 0);
+        for (int u = 1; u <= V; u++) {
+            for (auto v : adj[u]) {
+                if (u < v) {
+                    int u_parent = find(u, parent);
+                    int v_parent = find(v, parent);
+                    if (u_parent == v_parent) {
+                        return true;
+                    } else {
+                        dfsUnion(u, v, parent, rank);
+                    }
                 }
             }
         }
         return false;
     }
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
+        vector<int> vis(n + 1, 0);
         vector<vector<int>> adj(n + 1);
-        for (auto& e : edges) {
-            int u = e[0];
-            int v = e[1];
+        for (auto it : edges) {
+            int u = it[0];
+            int v = it[1];
             adj[u].push_back(v);
-            adj[v].push_back(u);
-            vector<int> vis(n + 1, 0);
-            if (detectCycle(u, adj, vis)) {
-                return e;
+            if (!vis[u]) {
+                if (cycleDetect(u, n, vis, adj)) {
+                    return it;
+                }
             }
         }
         return {};
