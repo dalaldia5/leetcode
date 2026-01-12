54f7470a68cb084e4e0008a33467e63dc4d3084c
Dia Dalal <dia140706@gmail.com>
2026-01-12 22:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (41.61%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -12,22 +12,27 @@
  */
 class Solution {
 public:
+// morris traversal
     vector<int> preorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
         vector<int> preorder;
-        if (root == NULL) {
-            return preorder;
-        }
-        st.push(root);
-        while (!st.empty()) {
-            TreeNode* node = st.top();
-            st.pop();
-            preorder.push_back(node->val);
-            if (node->right != NULL) {
-                st.push(node->right);
-            }
-            if (node->left != NULL) {
-                st.push(node->left);
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                preorder.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* prev = curr->left;
+                while (prev->right && prev->right != curr) {
+                    prev = prev->right;
+                }
+                if (prev->right == NULL) {
+                    prev->right = curr;
+                    preorder.push_back(curr->val);
+                    curr = curr->left;
+                } else {
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
             }
         }
         return preorder;
