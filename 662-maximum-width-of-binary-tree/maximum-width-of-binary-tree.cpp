678e77a62f55750c1ffb9fc66b20fa0f12381dae
Dia Dalal <dia140706@gmail.com>
2026-01-18 10:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (41.95%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -1,32 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (!root)
-            return 0;
-        long long maxWidth = 0;
+        long long ans = INT_MIN;
         queue<pair<TreeNode*, long long>> q;
         q.push({root, 0});
-
         while (!q.empty()) {
-            int size = q.size();
+            long long size = q.size();
             long long mmin = q.front().second;
-            long long first = 0, last = 0;
-
-            for (int i = 0; i < size; i++) {
+            long long first = 0;
+            long long last = 0;
+            for (long long i = 0; i < size; i++) {
                 long long curr_idx = q.front().second - mmin;
                 TreeNode* node = q.front().first;
                 q.pop();
-                if (i == 0)
+                if (i == 0) {
                     first = curr_idx;
-                if (i == size - 1)
+                }
+                if (i == size - 1) {
                     last = curr_idx;
-                if (node->left)
+                }
+                if (node->left) {
                     q.push({node->left, curr_idx * 2 + 1});
-                if (node->right)
+                }
+                if (node->right) {
                     q.push({node->right, curr_idx * 2 + 2});
+                }
             }
-            maxWidth = max(maxWidth, last - first + 1);
+            ans = max(ans, last - first + 1);
         }
-        return maxWidth;
+        return ans;
     }
 };
