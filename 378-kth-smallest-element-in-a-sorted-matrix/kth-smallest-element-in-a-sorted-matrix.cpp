4bba22acf58531a57412d37d2eed2feb4f52ed14
Dia Dalal <dia140706@gmail.com>
2026-04-09 00:40:53 +0530

    Time: 8 ms (35.59%) | Memory: 18.2 MB (51.55%) - LeetSync

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int> maxHeap;
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                maxHeap.push(matrix[i][j]);
+                if (maxHeap.size() > k) {
+                    maxHeap.pop();
+                }
+            }
+        }
+        return maxHeap.top();
+    }
+};
