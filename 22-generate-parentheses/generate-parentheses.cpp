cb5334a9b7696adf69c2287da1462c25d4e4006d
Dia Dalal <dia140706@gmail.com>
2026-04-15 22:38:07 +0530

    Time: 3 ms (69.83%) | Memory: 15.7 MB (34.30%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    vector<string> result;
-    void generatePattern(string currStr, int open, int close, int n) {
+    void generatePattern(string currStr, int open, int close, int n, vector<string>& ans) {
+        
         if (open == n && close == n) {
-            result.push_back(currStr);
+            ans.push_back(currStr);
             return;
         }
         if (open < n) {
-            generatePattern(currStr + "(", open + 1, close, n);
+            generatePattern(currStr + "(", open + 1, close, n, ans);
         }
         if (close < open) {
-            generatePattern(currStr + ")", open, close + 1, n);
+            generatePattern(currStr + ")", open, close + 1, n, ans);
         }
     }
     vector<string> generateParenthesis(int n) {
-        generatePattern("", 0, 0, n);
-        return result;
+        vector<string> ans;
+        generatePattern("", 0, 0, n, ans);
+        return ans;
     }
 };
